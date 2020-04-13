82aa97d3119c4e6f72ef6200639ee27e17af9392
Michał Wowk <michal.wowk@dev-chop-chop.org>
2020-04-13 19:55:10 +0200

    update

--- dist/intersection-observer-polyfill.aa6cbab4.js
+++ dist/intersection-observer-polyfill.aa6cbab4.js
@@ -871,7 +871,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55553" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58835" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
